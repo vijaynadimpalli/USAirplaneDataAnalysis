9bb768cf7cee965775d01ba3938621a755c3be7a
vijaynadimpalli <vijay.nadimpalli@gmail.com>
2020-11-25 16:04:58 +0530

    Create dtypeconverter.py

---
+++ dtypeconverter.py
@@ -0,0 +1,39 @@
+def floatconversion(data):
+ floatcol=[X for X in data.dtypes.index if data.dtypes[X]=='float64']
+ for col in floatcol:
+  data.loc[:,col]=pd.to_numeric(data.loc[:,col],downcast='float')
+ return data 
+ 
+ 
+def intconversion(data):
+ intcol=[X for X in data.dtypes.index if data.dtypes[X]=='int64']
+ for col in intcol:
+  data.loc[:,col]=pd.to_numeric(data.loc[:,col],downcast='integer')
+ return data 
+ 
+def catconversion(data):
+ catcol=[X for X in data.dtypes.index if data.dtypes[X]=='object']
+ for col in catcol:
+  num_unique_values = len(data[col].unique())
+  num_total_values = len(data[col])
+  if num_unique_values / num_total_values < 0.5:
+    data.loc[:,col] = data[col].astype('category')
+  else:
+    data.loc[:,col] = data[col]
+ return data 
+ 
+ 
+def dtypeconversion(data):
+ floatconversion(data)
+ intconversion(data)
+ catconversion(data)
+ return data 
+ 
+ 
+def main():
+    df=dtypeconversion(df) #df is your dataframe which needs dtype conversion.
+    colnames = dtypes.index
+    types = [i.name for i in df.dtypes.values]
+    column_types = dict(zip(colnames, types))
+    print(types)
+    return column_types
